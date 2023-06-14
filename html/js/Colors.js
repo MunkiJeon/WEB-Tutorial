4c6e3d435fcb7d4e2dd33f11e6b0fb01c00ca02a
munki07 <jmk070994@gmail.com>
2023-06-14 14:45:46 +0900

    V1.4 Html+CSS+JavaScript

---
+++ html/js/Colors.js
@@ -0,0 +1,37 @@
+var setColor = {
+    ModeSetColor:function (text_c,back_c,link_c){
+        document.querySelector('body').style.color = text_c;
+        document.querySelector('body').style.backgroundColor = back_c;
+
+        var alist = document.querySelectorAll('a');
+            var i = 0;
+            while(i<alist.length){
+                alist[i].style.color = link_c;
+                i = i+1;
+            }
+    }  
+}
+// function ModeSetColor(text_c,back_c,link_c){
+//     document.querySelector('body').style.color = text_c;
+//     document.querySelector('body').style.backgroundColor = back_c;
+
+//     var alist = document.querySelectorAll('a');
+//         var i = 0;
+//         while(i<alist.length){
+//             alist[i].style.color = link_c;
+//             i = i+1;
+//         }
+// }
+
+function modeChange(self){
+    var target = document.querySelector('body');
+    if( self.value === 'day' ){
+        self.value = 'night';
+        setColor.ModeSetColor('white','black','white');
+        // ModeSetColor('white','black','white');
+    }else{
+        self.value = 'day';
+        setColor.ModeSetColor('black','white','black');
+        // ModeSetColor('black','white','black');
+    }
+}
