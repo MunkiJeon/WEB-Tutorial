e9f611ddded6de433710919ed92b8c1ffffe4251
munki07 <jmk070994@gmail.com>
2023-06-14 15:16:16 +0900

    V1.5 BasicWebSite

--- html/js/Colors.js
+++ html/js/Colors.js
@@ -1,14 +1,16 @@
 var setColor = {
     ModeSetColor:function (text_c,back_c,link_c){
-        document.querySelector('body').style.color = text_c;
-        document.querySelector('body').style.backgroundColor = back_c;
-
-        var alist = document.querySelectorAll('a');
-            var i = 0;
-            while(i<alist.length){
-                alist[i].style.color = link_c;
-                i = i+1;
-            }
+//        document.querySelector('body').style.color = text_c;
+//        document.querySelector('body').style.backgroundColor = back_c;
+        $('body').css('color', text_c);
+        $('body').css('backgroundColor', back_c);
+//        var alist = document.querySelectorAll('a');
+//            var i = 0;
+//            while(i<alist.length){
+//                alist[i].style.color = link_c;
+//                i = i+1;
+//            }
+        $('a').css('color',link_c);
     }  
 }
 // function ModeSetColor(text_c,back_c,link_c){
